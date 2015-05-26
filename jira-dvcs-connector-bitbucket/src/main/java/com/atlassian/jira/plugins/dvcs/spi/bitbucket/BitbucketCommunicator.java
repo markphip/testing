1169f185cb175bd672cd8071e62ea1d90bf116e2
Ian Dick <idick@atlassian.com>
2015-05-26 10:16:31 +1000

    FUSE-2468: don't mix jsr and spring annotations

--- jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/BitbucketCommunicator.java
+++ jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/BitbucketCommunicator.java
@@ -57,7 +57,6 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
@@ -99,8 +98,7 @@ public class BitbucketCommunicator implements DvcsCommunicator
     @Resource
     private SyncDisabledHelper syncDisabledHelper;
     
-    @Resource
-    @Qualifier ("deferredBitbucketLinker") 
+    @Resource (name = "deferredBitbucketLinker")
     private BitbucketLinker bitbucketLinker;
 
     @Autowired
