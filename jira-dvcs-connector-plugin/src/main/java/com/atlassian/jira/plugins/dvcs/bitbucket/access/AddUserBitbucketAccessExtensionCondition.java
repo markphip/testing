fec69a2dafae3a4d4114b8889a034b33d4e86b75
Eric Sukmajaya <esukmajaya@atlassian.com>
2015-06-15 14:32:43 +1000

    FUSE-2520 Added AddUserBitbucketAccessExtensionCondition.java back. We need to do this even though we already have the same check in the Velocity template (to not show anything when there are no BB team configured with defaults) because we'll be making changes in JIRA to show the application selector when the total number of JIRA applications and displayable web panel descriptor exceed one. To get the correct number of displayable web panel descriptor, we need to add back this condition and use it to guard the web panel.

---
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/bitbucket/access/AddUserBitbucketAccessExtensionCondition.java
@@ -0,0 +1,31 @@
+package com.atlassian.jira.plugins.dvcs.bitbucket.access;
+
+import com.atlassian.plugin.web.Condition;
+
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Checks whether there is at least one Bitbucket team with default groups.
+ *
+ * When this condition is satisfied, we should show the Bitbucket access extension in the add user page.
+ */
+public class AddUserBitbucketAccessExtensionCondition implements Condition
+{
+    private final BitbucketTeamService bitbucketTeamService;
+
+    public AddUserBitbucketAccessExtensionCondition(BitbucketTeamService bitbucketTeamService)
+    {
+        this.bitbucketTeamService = checkNotNull(bitbucketTeamService);
+    }
+
+    @Override
+    public void init(Map<String, String> params) {}
+
+    @Override
+    public boolean shouldDisplay(Map<String, Object> context)
+    {
+        return bitbucketTeamService.getTeamsWithDefaultGroups().size() > 0;
+    }
+}
