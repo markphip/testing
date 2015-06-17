071900e4b2377d10fbe49d19055170078a574b7c
Eric Sukmajaya <esukmajaya@atlassian.com>
2015-06-17 18:48:25 +1000

    FUSE-2520 Introduced TestQUnit.java to run our qunit tests.

---
+++ jira-dvcs-connector-plugin/src/test/java/it/com/atlassian/jira/plugins/dvcs/TestQUnit.java
@@ -0,0 +1,35 @@
+package it.com.atlassian.jira.plugins.dvcs;
+
+import com.atlassian.jira.pageobjects.BaseJiraWebTest;
+import com.atlassian.jira.pageobjects.JiraTestedProduct;
+import com.atlassian.pageobjects.TestedProductFactory;
+import com.atlassian.qunit.test.runner.QUnitPageObjectsHelper;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestQUnit extends BaseJiraWebTest
+{
+    private final File outputDirectory;
+    private final JiraTestedProduct product = TestedProductFactory.create(JiraTestedProduct.class);
+
+    public TestQUnit()
+    {
+        String location = System.getProperty("jira.qunit.testoutput.location");
+
+        if (StringUtils.isEmpty(location)) {
+            System.err.println("jira.qunit.testoutput.location is not defined, writing TestQUnit output to tmp.");
+            location = System.getProperty("java.io.tmpdir");
+        }
+
+        outputDirectory = new File(location);
+    }
+
+    @Test
+    public void runJustOurTest() throws Exception
+    {
+        QUnitPageObjectsHelper helper = new QUnitPageObjectsHelper(outputDirectory, product.getPageBinder(), "/qunit");
+        helper.runTests(QUnitPageObjectsHelper.testFilePathContains("com.atlassian.jira.plugins.jira-bitbucket-connector-plugin"));
+    }
+}
