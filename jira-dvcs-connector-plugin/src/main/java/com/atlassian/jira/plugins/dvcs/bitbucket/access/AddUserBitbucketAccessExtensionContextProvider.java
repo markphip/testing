ab0be26711e719a512f1448711c29bcc82f58874
Ahmed Fouad <afouad@atlassian.com>
2015-07-02 12:57:42 +1000

    create base context provider for all bitbucket access extension components

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/bitbucket/access/AddUserBitbucketAccessExtensionContextProvider.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/bitbucket/access/AddUserBitbucketAccessExtensionContextProvider.java
@@ -24,34 +24,8 @@ import static java.util.Collections.emptyList;
 /**
  * Context provider for the Bitbucket access extension on the add user page.
  */
-public class AddUserBitbucketAccessExtensionContextProvider implements ContextProvider
+public class AddUserBitbucketAccessExtensionContextProvider extends BitbucketAccessExtensionContextProvider implements ContextProvider
 {
-    /**
-     * This context key points to JIRA's base url. We use this so we can specify a link to the 'DVCS accounts' page.
-     */
-    @VisibleForTesting
-    static final String CONTEXT_KEY_JIRA_BASE_URL = "jiraBaseUrl";
-
-    /**
-     * When we have more than 3 Bitbucket teams, we list the first 3 teams and wrote ", and N other". This
-     * context key points to the value for N.
-     */
-    @VisibleForTesting
-    static final String CONTEXT_KEY_MORE_COUNT = "moreCount";
-
-    /**
-     * This context key points to a collection of team names that are not displayed and will be displayed in
-     * the inline dialog when the user clicks on the "N other" link.
-     */
-    @VisibleForTesting
-    static final String CONTEXT_KEY_MORE_TEAMS = "moreTeams";
-
-    /**
-     * This context key points to the full collection of team names. We take the first three and display them.
-     */
-    @VisibleForTesting
-    static final String CONTEXT_KEY_TEAMS_WITH_DEFAULT_GROUPS = "teamsWithDefaultGroups";
-
     private static final String ORGANIZATION_GROUP_PAIR_SEPARATOR = ";";
 
     private static final String ORGANIZATION_GROUP_SEPARATOR = ":";
@@ -62,48 +36,19 @@ public class AddUserBitbucketAccessExtensionContextProvider implements ContextPr
     @VisibleForTesting
     static final String REQUIRED_WEB_RESOURCE_COMPLETE_KEY = "com.atlassian.jira.plugins.jira-bitbucket-connector-plugin:add-user-bitbucket-access-extension-resources";
 
-    private static final int TEAMS_DISPLAY_THRESHOLD = 3;
-
-    private final ApplicationProperties applicationProperties;
-
-    private final BitbucketTeamService bitbucketTeamService;
-
     private final PageBuilderService pageBuilderService;
 
     public AddUserBitbucketAccessExtensionContextProvider(@ComponentImport ApplicationProperties applicationProperties,
             BitbucketTeamService bitbucketTeamService, @ComponentImport PageBuilderService pageBuilderService)
     {
-        this.applicationProperties = checkNotNull(applicationProperties);
-        this.bitbucketTeamService = checkNotNull(bitbucketTeamService);
+        super(applicationProperties, bitbucketTeamService);
         this.pageBuilderService = checkNotNull(pageBuilderService);
     }
 
     @Override
     public void init(Map<String, String> params) {}
 
-    @Override
-    public Map<String, Object> getContextMap(Map<String, Object> context)
-    {
-        List<Organization> bitbucketTeamsWithDefaultGroups = bitbucketTeamService.getTeamsWithDefaultGroups();
-        List<String> bitbucketTeamNames = transform(bitbucketTeamsWithDefaultGroups, new Function<Organization, String>()
-        {
-            @Override
-            public String apply(Organization organization)
-            {
-                return organization.getName();
-            }
-        });
-
-        requireResourcesAndData(bitbucketTeamsWithDefaultGroups);
-        return ImmutableMap.of(
-                CONTEXT_KEY_JIRA_BASE_URL, applicationProperties.getString(JIRA_BASEURL),
-                CONTEXT_KEY_MORE_COUNT, max(0, bitbucketTeamsWithDefaultGroups.size() - TEAMS_DISPLAY_THRESHOLD),
-                CONTEXT_KEY_MORE_TEAMS, inlineDialogContent(bitbucketTeamNames),
-                CONTEXT_KEY_TEAMS_WITH_DEFAULT_GROUPS, bitbucketTeamNames
-        );
-    }
-
-    private void requireResourcesAndData(List<Organization> bitbucketTeamsWithDefaultGroups)
+    protected void requireResourcesAndData(final List<Organization> bitbucketTeamsWithDefaultGroups)
     {
         if (bitbucketTeamsWithDefaultGroups.isEmpty())
         {
@@ -113,16 +58,6 @@ public class AddUserBitbucketAccessExtensionContextProvider implements ContextPr
         String inviteToGroups = inviteToGroups(bitbucketTeamsWithDefaultGroups);
         pageBuilderService.assembler().resources().requireWebResource(REQUIRED_WEB_RESOURCE_COMPLETE_KEY);
         pageBuilderService.assembler().data().requireData(REQUIRED_DATA_BITBUCKET_INVITE_TO_GROUPS_KEY, inviteToGroups);
-    }
-
-    private List<String> inlineDialogContent(List<String> bitbucketTeamNames)
-    {
-        if (bitbucketTeamNames.size() > TEAMS_DISPLAY_THRESHOLD)
-        {
-            return bitbucketTeamNames.subList(TEAMS_DISPLAY_THRESHOLD, bitbucketTeamNames.size());
-        }
-
-        return emptyList();
     }
 
     private String inviteToGroups(List<Organization> organizations)
